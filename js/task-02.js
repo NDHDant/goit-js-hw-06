71fc4631bac2a444e22dae8f533958fcd57aeb00
Yaroslav Nedohoda <y.nedogoda@gmail.com>
2022-10-28 22:16:22 +0300

    add task2/3

--- js/task-02.js
+++ js/task-02.js
@@ -11,3 +11,12 @@ const ingredientsEl = document.querySelector("#ingredients");
 
 console.log(ingredientsEl);
 console.log(ingredients);
+
+const listRefs = ingredients.map((ingredient) => {
+  const listRef = document.createElement("li");
+  listRef.textContent = ingredient;
+  listRef.classList.add("item");
+  return listRef;
+});
+
+ingredientsEl.append(...listRefs);
