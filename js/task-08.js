681a9a0313beb9580b71b4f58ddb0d825d11918d
Yaroslav Nedohoda <y.nedogoda@gmail.com>
2022-11-01 13:39:48 +0200

    add task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formLog = document.querySelector(`.login-form`);
+console.log(formLog);
 
+formLog.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+	evt.preventDefault();
+	
+	const formElements = evt.currentTarget.elements;
+	const email = formElements.email.value;
+	const pass = formElements.password.value;
+
+	if (email === "" || pass === "") {
+		window.alert("Заповніть всі поля");
+	}
+
+	const formData = { email, pass };
+	console.log(formData);
+
+	evt.currentTarget.reset();
+}
