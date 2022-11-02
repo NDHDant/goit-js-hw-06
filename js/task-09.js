38dcf1e34c731a38c13898b417e757cf525663fb
Yaroslav Nedohoda <y.nedogoda@gmail.com>
2022-11-02 15:43:00 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector(`body`);
+const changeBtn = document.querySelector(`.change-color`);
+const actualColor = document.querySelector(`.color`);
+
+changeBtn.addEventListener(`click`, onClick);
+
+function onClick() {
+	bodyEl.style.backgroundColor = getRandomHexColor();
+	actualColor.textContent = bodyEl.style.backgroundColor;
+}
